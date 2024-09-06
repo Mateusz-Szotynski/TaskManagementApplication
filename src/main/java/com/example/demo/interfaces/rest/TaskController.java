d8cbcf5014267ade418b8518c8561d8560ae5166
Mateusz <mateusz.szotynski3@gmail.com>
2024-09-07 00:15:22 +0200

    created TaskControllerTests and made test for returnign a list of Tasks with particular title

--- src/main/java/com/example/demo/interfaces/rest/TaskController.java
+++ src/main/java/com/example/demo/interfaces/rest/TaskController.java
@@ -1,6 +1,26 @@
 package com.example.demo.interfaces.rest;
 
-import org.springframework.stereotype.Controller;
+import com.example.demo.application.TaskService;
+import com.example.demo.domain.Task;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<Task>> findTasksByTitle(@RequestParam String title) {
+        return ResponseEntity.ok(taskService.findTasksByTitle(title));
+    }
 }
--- src/test/java/com/example/demo/domain/TaskControllerTests.java
+++ src/test/java/com/example/demo/domain/TaskControllerTests.java
@@ -1,35 +1,26 @@
 package com.example.demo.domain;
 
 import com.example.demo.application.TaskService;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import com.example.demo.interfaces.rest.TaskController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.assertArg;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 
 @Testcontainers
 @ExtendWith(MockitoExtension.class)
-public class TaskServiceTests {
+public class TaskControllerTests {
 
     private final String happyTitle = "testTitle";
     private final String happyDescription = "testDescription";
@@ -38,25 +29,27 @@ public class TaskServiceTests {
     private final LocalDate happyDueToDate = LocalDate.now().plusDays(3);
 
     @Mock
-    private TaskRepository taskRepository;
-
-    @InjectMocks
     private TaskService taskService;
 
+    @InjectMocks
+    private TaskController taskController;
+
     @Test
-    @DisplayName("Finds and returns found tasks by title")
-    void returnTasksByTitle() {
-        when(taskRepository.findByTitle(happyTitle)).thenReturn(List.of(
+    @DisplayName("Returns all tasks with particular title")
+    void findTasksByTitle() {
+        when(taskService.findTasksByTitle(happyTitle)).thenReturn(List.of(
                 new Task(happyTitle, happyDescription, happyDueToDate),
                 new Task(happyTitle, happyDescription, happyHighPriority, happyDueToDate)
         ));
 
-        List<Task> taskList = taskService.findTasksByTitle(happyTitle);
+        ResponseEntity<List<Task>> taskList = taskController.findTasksByTitle(happyTitle);
 
         assertAll(() -> {
-            assertFalse(taskList.isEmpty());
-            assertEquals(2, taskList.size());
-            assertTrue(taskList.stream().allMatch((e) -> e.getTitle().contentEquals(happyTitle)));
+            assertFalse(taskList.getBody().isEmpty());
+            assertEquals(HttpStatus.OK, taskList.getStatusCode());
+            assertEquals(2, taskList.getBody().size());
+            assertTrue(taskList.getBody().stream().allMatch((e) -> e.getTitle().contentEquals(happyTitle)));
         });
+
     }
 }
